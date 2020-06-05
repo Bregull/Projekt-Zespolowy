cadd9ea1ff6979e2db8ddaad676d8c8f8768fd61
polakrupa <61592048+polakrupa@users.noreply.github.com>
2020-06-05 11:59:49 +0200

    Update views.py

--- Katalog/Projektzespolowy/zespol/zespol/views.py
+++ Katalog/Projektzespolowy/zespol/zespol/views.py
@@ -9,15 +9,6 @@ from subprocess import run,PIPE
 def button(request):
     return render(request,'home.html')
 
-def output(request):
-
-    data=requests.get("https://www.google.com/")
-
-    print(data.text)
-
-    data=data.text
-
-    return render(request,'home.html',{'data':data})
 
 def external(request):
 
