bddbe99d3383d4904e020a5f62587995890bb811
Bregul <bregul58@gmail.com>
2020-06-18 20:24:24 +0200

    Update main.py

--- Katalog/main.py
+++ Katalog/main.py
@@ -1,5 +1,4 @@
-#from data_input import *
-import record_import_wav
+from data_input import *
 from plot_spectrograms import *
 import convolutional_neural_networks
 
@@ -15,8 +14,7 @@ file_name - nazwa pliku
 '''
 
 
-#fs, samples, file_name = data_input()  # wybieramy plik który chcemy przeanalizować
-fs, samples, file_name, file_path = record_import_wav.import_wav()
+fs, samples, file_name = data_input()  # wybieramy plik który chcemy przeanalizować
 spectrogram(fs, samples, file_name)  # plotuje i zapisuje do .png spektrogram
 mel_spectrogram(fs, samples, file_name)  # plotuje i zapisuje do .png spektrogram melowy
 name = convolutional_neural_networks.print_prediction(f'{file_path}/{file_name}')
