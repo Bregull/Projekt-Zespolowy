afc47a0eb191eacffa410cb25b1e86bfb639c6f8
Bregul <bregul58@gmail.com>
2020-06-18 20:25:15 +0200

    Update main.py

--- Katalog/main.py
+++ Katalog/main.py
@@ -14,7 +14,7 @@ file_name - nazwa pliku
 '''
 
 
-fs, samples, file_name = data_input()  # wybieramy plik który chcemy przeanalizować
+fs, samples, file_name, file_path = data_input()  # wybieramy plik który chcemy przeanalizować
 spectrogram(fs, samples, file_name)  # plotuje i zapisuje do .png spektrogram
 mel_spectrogram(fs, samples, file_name)  # plotuje i zapisuje do .png spektrogram melowy
 name = convolutional_neural_networks.print_prediction(f'{file_path}/{file_name}')
